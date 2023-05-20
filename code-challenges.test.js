1b8b21ba09d8c852a06e6eac74542151e5d7eb16
miguelosvaldo <miguel.o.molina@icloud.com>
2023-05-20 16:37:48 -0700

    Week 2 assessment time

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,9 +12,27 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
-// Pseudo code:
+// Pseudo code: 
+        
+        // Going to creat function named multBy3
+        // function takes in one array 
+        //using .map going to iterate over given array at given time
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("multBy3", () => {
+  it("returns an array with all the numbers multiplied by 3.", () => {
+const numbersArray1 = [6, 7, 8, 9, 10]
+// Expected output: [18, 21, 24, 27, 30]
+const numbersArray2 = [24, 27, 30, 33, 36]
+// Expected output: [72, 81, 90, 99, 108]
+    expect(multBy3(numbersArray1 )).toEqual([18, 21, 24, 27, 30])
+    expect(multBy3(numbersArray2 )).toEqual([72, 81, 90, 99, 108])
+
+  })
+})
+        //Problem Example first test -  ReferenceError: multBy3 is not defined
+        //Problem Example passed second test
 
 const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
@@ -23,9 +41,31 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // b) Create the function that makes the test pass.
 
+const multBy3 = (array) => {
+    return array.map(value => value * 3)
+}
+
+
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("objDivBy3", () => {
+    it("decides if the number inside it is evenly divisible by three or not", () => {
+        const object1 = { number: 15 }
+        // Expected output: "15 is divisible by three"
+        const object2 = { number: 0 }
+        // Expected output: "0 is divisible by three"
+        const object3 = { number: -7 }
+        // Expected output: "-7 is not divisible by three"
+      expect(objDivBy3(object1)).toEqual("15 is divisible by three")
+      expect(objDivBy3(object2)).toEqual("0 is divisible by three")
+      expect(objDivBy3(object3)).toEqual("-7 is not divisible by three")
+    
+    })
+  })
+        //Problem 1 fist test -  ReferenceError: objDivBy3 is not defined
+        //Problem 1 passed second test
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -35,12 +75,42 @@ const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
+        
+        // Pseudo code:
+                
+                 // Going to creat a function named objDivBy3 and will take an object as an argument 
+                // Going to write if statement to check if given number is divisible by 3 
+                 //Going to use string interpolation to keep the function dynamic with given value
 
-// Pseudo code:
+const objDivBy3 = (obj) => {
+    if (obj.number % 3 == 0){
+        return `${obj.number} is divisible by three`
+    } else if (obj.number % 3 !== 0) {
+        return`${obj.number} is not divisible by three`
+    }
+}
+
+
+    
+  
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+
+describe("capsOnAll", () => {
+    it("returns an array with all the words capitalized", () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+      expect(capsOnAll(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+      expect(capsOnAll(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+     })
+  })
+            //Problem 2 fist test - ReferenceError: capsOnAll is not defined
+            //Problem 2 passed second test 
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -49,11 +119,45 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+        // Pseudo code:
+                // Create a function called capsOnAll that takes in an array as an argument
+                //Going to use .map() to iterate over array
+                //Going to make two variable, one to separate first letter from rest of the word, by using charAt(0) to target first letter, then .toUpperCase()to get capital letter
+                //Going to slice the word from second character to the rest of the word by using .splice, and lower casing the rest by using .toLowerCase
+                // going to return both variables by adding them together
+        
+
+               
+const capsOnAll = (arr) => {
+    return arr.map(value => {
+ let firstLetter = value.charAt(0).toUpperCase()
+ let restWord = value.slice(1).toLowerCase()
+    return firstLetter + restWord
+})
+}
+     
+
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("firstVowel", () => {
+    it("logs the index of the first vowel", () => {
+        const vowelTester1 = "learn"
+        // Expected output: 1
+        const vowelTester2 = "academy"
+        // Expected output: 0
+        const vowelTester3 = "challenges"
+        // Expected output: 2
+      expect(firstVowel(vowelTester1)).toEqual(1)
+       expect(firstVowel(vowelTester2)).toEqual(0)
+       expect(firstVowel(vowelTester3)).toEqual(2)
+     })
+  })
+            //Problem 3 first test - ReferenceError: firstVowel is not defined
+            //Problem 3 
+
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -64,4 +168,12 @@ const vowelTester3 = "challenges"
 
 // b) Create the function that makes the test pass.
 
+const firstVowel = (str) => {
+    return str.indexOf(str[1])
+
+}
+
+
 // Pseudo code:
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A parameter is the place holder for a function, while an argument is the value that is passed into the parameter.
 
-Researched answer:
+Researched answer: The values that are declared within a function when the function is called are known as an argument. Whereas, the variables that are defined when the function is declared are known as a parameter
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: The predefined parameters are Value, index, and array. The optional ones are index and array.
 
-Researched answer:
+Researched answer: currentValue	Required. The value of the current element. index	Optional. The index of the current element. arr	Optional. The array of the current element.
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: Map returns an array with the same length, and filter returns an array with only the needed elements.
 
-Researched answer:
+Researched answer: map returns the same number of elements as the original, but the element value will be transformed in some way and filter will return the same or less number of elements than the original but not change the original elements’ values.
 
 1. What is iteration?
 
-Your answer:
+Your answer: Iteration is the evaluation of each given element until a certain condition is met.
 
-Researched answer:
+Researched answer: A sequence of instructions or code being repeated until a specific end result is achieved. 
 
 1. What is the difference between a function and a method?
 
-Your answer:
+Your answer: a Function is a block of code that can be reused in the future, while a method is a built in behavior that will facilitate the code execution.
 
-Researched answer:
+Researched answer: A function doesn't need any object and is independent, while the method is a function, which is linked with any object. We can directly call the function with its name, while the method is called by the object's name. Function is used to pass or return the data, while the method operates the data in a class
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: I honestly don't recall. But i believe it is connecting multiple methods together.
 
-Researched answer:
+Researched answer: the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Component:
+1. Component: Are like functions, but are pre established and very important to a given project.
 
-2. Spread operator:
+2. Spread operator: Helps to spread the elements into a smaller presentation.
 
-3. React state:
+3. React state: Allow us to manage data in an application.
 
-4. React props:
+4. React props: Are properties of an object in react.
 
-5. DOM events:
+5. DOM events: Any interaction the user is having with the web page.
